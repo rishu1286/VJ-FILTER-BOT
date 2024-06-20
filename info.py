7c160c8be745ca919ae75d2a8691e4e6a75a668b
The-Roxxstar <followinstagram296@gmail.com>
2024-06-20 15:01:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -102,7 +102,7 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://zxlink.in')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'zxlink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '1b4bd7a1903b890dbbcecdfcd390f415745767c3')
 
 # Others
