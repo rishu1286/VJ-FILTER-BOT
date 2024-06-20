1fc230a2b85821dc0f288bba7727776b6744305f
The-Roxxstar <followinstagram296@gmail.com>
2024-06-20 12:37:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -102,8 +102,8 @@ VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl
 
 # Shortlink Info
 SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://zxlink.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1b4bd7a1903b890dbbcecdfcd390f415745767c3')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
