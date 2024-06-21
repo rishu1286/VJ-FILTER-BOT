75cb1d0bcaea7b5bb35b04ba07e6a7f7bfedd3be
The-Roxxstar <followinstagram296@gmail.com>
2024-06-21 12:52:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -685,13 +685,13 @@ Also your tutorial will be Added Your Group..."""
     DEVELOPER_TXT = """
 special Thanks To ❤️ Developers -
 
--Dev 1 [Owner of this bot ]<a href='https://t.me/KingVj01'>VJ</a>
+-Dev 1 [Owner of this bot ]<a href='https://t.me/Royal_X_RoxStar'>VJ</a>
 
 -Dev 2 <a href='https://t.me/vjbots_bot'>VJ</a>
 
 -Dev 3 <a href='https://t.me/vj_botz>VJ Botz</a>
 
-- Dev 4 <a href='https://t.me/vj_bots'>TEAM VJ</a>
+- Dev 4 <a href='https://t.me/PopCorn_Movie_Group'>TEAM VJ</a>
 """
 
     RENAME_TXT = """
